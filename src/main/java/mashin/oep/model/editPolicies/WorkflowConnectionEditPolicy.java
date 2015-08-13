6fc280fefc14845b82ade9e8dd0bac54237e6120
Ahmed Mahran <ahmahran@gmail.com>
2015-08-14 00:16:12 +0200

    Add Connection delete command

--- src/main/java/mashin/oep/model/editPolicies/WorkflowConnectionEditPolicy.java
+++ src/main/java/mashin/oep/model/editPolicies/WorkflowConnectionEditPolicy.java
@@ -1,5 +1,8 @@
 package mashin.oep.model.editPolicies;
 
+import mashin.oep.model.WorkflowConnection;
+import mashin.oep.model.commands.connection.ConnectionDeleteCommand;
+
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.editpolicies.ConnectionEditPolicy;
 import org.eclipse.gef.requests.GroupRequest;
@@ -8,7 +11,7 @@ public class WorkflowConnectionEditPolicy extends ConnectionEditPolicy {
 
   @Override
   protected Command getDeleteCommand(GroupRequest request) {
-    return null;
+    return new ConnectionDeleteCommand((WorkflowConnection) getHost().getModel());
   }
 
 }
