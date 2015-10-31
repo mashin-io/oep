16d7a525db0d72af7a714897bc1c7f8b73c313c2
Ahmed Mahran <ahmahran@gmail.com>
2015-10-31 22:42:25 +0200

    Use SLA in action nodes

--- plugin/src/main/java/io/mashin/oep/model/node/action/ActionNode.java
+++ plugin/src/main/java/io/mashin/oep/model/node/action/ActionNode.java
@@ -8,6 +8,7 @@ import io.mashin.oep.model.connection.WorkflowConnection;
 import io.mashin.oep.model.connection.WorkflowConnectionDummyEndPoint;
 import io.mashin.oep.model.connection.WorkflowConnectionEndPoint;
 import io.mashin.oep.model.node.Node;
+import io.mashin.oep.model.property.SLAPropertyElement;
 import io.mashin.oep.model.property.TextPropertyElement;
 import io.mashin.oep.model.property.filter.SchemaVersionRangeFilter;
 import io.mashin.oep.model.terminal.FanInTerminal;
@@ -23,6 +24,7 @@ public abstract class ActionNode extends Node {
   public static final String PROP_NODE_CRED          = "prop.node.action.cred";
   public static final String PROP_NODE_RETRYMAX      = "prop.node.action.retry-max";
   public static final String PROP_NODE_RETRYINTERVAL = "prop.node.action.retry-interval";
+  public static final String PROP_NODE_SLA           = "prop.node.action.sla";
   
   protected FanInTerminal fanInTerminal;
   protected SingleOutputTerminal okSingleOutputTerminal;
@@ -31,6 +33,7 @@ public abstract class ActionNode extends Node {
   protected TextPropertyElement cred;
   protected TextPropertyElement retryMax;
   protected TextPropertyElement retryInterval;
+  protected SLAPropertyElement  sla;
   
   public ActionNode(Workflow workflow) {
     this(workflow, null);
@@ -57,6 +60,13 @@ public abstract class ActionNode extends Node {
     retryInterval = new TextPropertyElement(PROP_NODE_RETRYINTERVAL, "Retry Interval",
         new SchemaVersionRangeFilter(SchemaVersion.V_0_3, SchemaVersion.V_ANY, workflow));
     addPropertyElement(retryInterval);
+    
+    sla = new SLAPropertyElement(PROP_NODE_SLA, "SLA", workflow,
+        new SchemaVersionRangeFilter(SchemaVersion.V_0_2, SchemaVersion.V_ANY, workflow)
+        .and(pe -> SchemaVersion.V_0_1.equals(workflow.getSLAVersion())),
+        new SchemaVersionRangeFilter(SchemaVersion.V_0_5, SchemaVersion.V_ANY, workflow)
+        .and(pe -> SchemaVersion.V_0_2.equals(workflow.getSLAVersion())));
+    addPropertyElement(sla);
   }
   
   @Override
@@ -84,6 +94,8 @@ public abstract class ActionNode extends Node {
         okSingleOutputTerminal.getConnections(), nodeElement, "ok", "to");
     XMLWriteUtils.writeConnectionsAsElementWithAttribute(
         errSingleOutputTerminal.getConnections(), nodeElement, "error", "to");
+    
+    XMLWriteUtils.writeSLAProperty(workflow, sla, nodeElement);
   }
   
   @Override
@@ -93,6 +105,7 @@ public abstract class ActionNode extends Node {
     XMLReadUtils.initTextPropertyFrom(cred, hpdlNode, "@cred");
     XMLReadUtils.initTextPropertyFrom(retryMax, hpdlNode, "@retry-max");
     XMLReadUtils.initTextPropertyFrom(retryInterval, hpdlNode, "@retry-interval");
+    XMLReadUtils.initSLAPropertyFrom(sla, hpdlNode, "./sla:info");
     
     // read connections
     
